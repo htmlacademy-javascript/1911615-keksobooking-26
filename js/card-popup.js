e5c0a9f1127ce95b8cdeabcf3dc139e89c79ca76
Sergey <Maxxmell@yandex.ru>
2022-06-14 17:16:16 +0300

    card-generator function

--- js/card-popup.js
+++ js/card-popup.js
@@ -27,43 +27,48 @@ const popupPhotos = cardElement.querySelector('.popup__photos');
 const popupPhoto = cardElement.querySelector('.popup__photo');
 
 const randomObjects = generateObjects();
-const currentObject = randomObjects[getRandomNumber(0,randomObjects.length-1)];
 
-// Обязательные поля
-popupTitle.textContent = currentObject.offer.title;
-popupTextAddress.textContent = currentObject.offer.address;
-popupTextPrice.textContent =`${currentObject.offer.price} ₽/ночь`;
-popupType.textContent = TYPES_DICTIONARY[currentObject.offer.type];
-popupTextCapacity.textContent = `${currentObject.offer.rooms} комнаты для ${currentObject.offer.guests} гостей`;
-popupTextTime.textContent = `Заезд после ${currentObject.offer.checkin}, выезд до ${currentObject.offer.checkout}`;
-popupAvatar.src = currentObject.author.avatar;
+const createCard = (index) => {
+  const currentObject = randomObjects[index];
 
-// Необязательные поля
-popupDescription.textContent = currentObject.offer.description ? currentObject.offer.description : popupDescription.classList.add('hidden');
+  // Обязательные поля
+  popupTitle.textContent = currentObject.offer.title;
+  popupTextAddress.textContent = currentObject.offer.address;
+  popupTextPrice.textContent =`${currentObject.offer.price} ₽/ночь`;
+  popupType.textContent = TYPES_DICTIONARY[currentObject.offer.type];
+  popupTextCapacity.textContent = `${currentObject.offer.rooms} комнаты для ${currentObject.offer.guests} гостей`;
+  popupTextTime.textContent = `Заезд после ${currentObject.offer.checkin}, выезд до ${currentObject.offer.checkout}`;
+  popupAvatar.src = currentObject.author.avatar;
 
-const featureModifiers = currentObject.offer.features.map((objectFeature) =>`popup__feature--${objectFeature}`);
-popupFeatures.forEach((feature) => {
-  const modifier = feature.classList[1];
+  // Необязательные поля
+  popupDescription.textContent = currentObject.offer.description ? currentObject.offer.description : popupDescription.classList.add('hidden');
 
-  if (!featureModifiers.includes(modifier)) {
-    feature.remove();
+  const featureModifiers = currentObject.offer.features.map((objectFeature) =>`popup__feature--${objectFeature}`);
+  popupFeatures.forEach((feature) => {
+    const modifier = feature.classList[1];
+
+    if (!featureModifiers.includes(modifier)) {
+      feature.remove();
+    }
+  });
+
+  const photosList = currentObject.offer.photos;
+  if (photosList.length === 1) {
+    popupPhoto.src = photosList[0];
   }
-});
-
-const photosList = currentObject.offer.photos;
-if (photosList.length === 1) {
-  popupPhoto.src = photosList[0];
-}
-else if (photosList.length > 1){
-  popupPhoto.src = photosList[0];
-  for (let i = 0; i < photosList.length-1;i++) {
-    const newPhoto = popupPhoto.cloneNode();
-    newPhoto.src = photosList[i+1];
-    popupPhotos.append(newPhoto);
+  else if (photosList.length > 1){
+    popupPhoto.src = photosList[0];
+    for (let i = 0; i < photosList.length-1;i++) {
+      const newPhoto = popupPhoto.cloneNode();
+      newPhoto.src = photosList[i+1];
+      popupPhotos.append(newPhoto);
+    }
   }
-}
-else {
-  popupPhotos.classList.add('hidden');
-}
+  else {
+    popupPhotos.classList.add('hidden');
+  }
 
-mapCanvas.appendChild(cardElement);
+  return cardElement;
+};
+
+mapCanvas.appendChild(createCard(getRandomNumber(0,randomObjects.length-1)));
