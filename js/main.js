a67d57676481c087b3a064cc16f1d84004a1adab
Sergey <Maxxmell@yandex.ru>
2022-07-13 13:23:38 +0300

    Добавит фильтрацию объявлений

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ import initMapForm from './map-form.js';
 import initAdForm from './ad-form.js';
 import {getAds, postAd} from './gateway.js';
 import showMessage from './message.js';
+import {debounce} from './utilities.js';
 
 const map = renderMap({
   center: [35.681729, 139.753927],
@@ -16,7 +17,7 @@ const adForm = initAdForm().setDisabled(true);
 map.whenReady(async () => {
   try {
     const ads = await getAds();
-    const replacePins = () => map.replacePins(ads);
+    const replacePins = debounce(() => map.replacePins(mapForm.filter(ads)));
     mapForm.setDisabled(false).on('change', replacePins).fire('change');
 
   } catch (exception) {
--- js/map-form.js
+++ js/map-form.js
@@ -46,6 +46,91 @@ function initMapForm() {
       mapFormElement.dispatchEvent(new Event(name));
 
       return this;
+    },
+
+    /**
+     * Проверит соответствие вида жилья.
+     * @param {string} type
+     */
+    testType(type) {
+      const {value} = mapFormElement['housing-type'];
+
+      return value === 'any' || value === type;
+    },
+
+    /**
+     * Проверит соответствие стоимости жилья.
+     * @param {string} price
+     */
+    testPrice(price) {
+      const {value} = mapFormElement['housing-price'];
+
+      if (value === 'any') {
+        return true;
+      } else if(value === 'high' && price >50000) {
+        return true;
+      } else if(value === 'middle' && price >=10000 && 50000 > price) {
+        return true;
+      }
+      else if(value === 'low' && price <10000) {
+        return true;
+      }
+      return false;
+    },
+
+    /**
+     * Проверит соответствие количества комнат.
+     * @param {string} rooms
+     */
+    testRooms(rooms) {
+      const {value} = mapFormElement['housing-rooms'];
+
+      return value === 'any' || value === rooms.toString();
+    },
+
+    /**
+     * Проверит соответствие количества гостей.
+     * @param {string} guests
+     */
+    testGuests(guests) {
+      const {value} = mapFormElement['housing-guests'];
+
+      return value === 'any' || value === guests.toString();
+    },
+
+    /**
+     * Проверит соответствие удобств.
+     * @param {string} features
+     */
+    testFeatures(features) {
+      const selectedElements = mapFormElement['housing-features'].querySelectorAll('input:checked');
+      const checkedValues = Array.prototype.map.call(selectedElements,(callback)=> callback.value);
+      if (checkedValues.length) {
+        if(features === undefined){
+          return false;
+        }
+        for(let i=0; i<checkedValues.length; i++){
+          if(!features.includes(checkedValues[i])){
+            return false;
+          }
+        }
+        return true;
+      }
+      return true;
+    },
+
+    /**
+     * Вернет объявления которые соответствуют текущим критериям.
+     * @param {Ad[]} ads
+     * @param {number} limit
+     */
+    filter(ads, limit = 10) {
+      return ads.filter((ad) => this.testType(ad.offer.type))
+        .filter((ad) => this.testPrice(ad.offer.price))
+        .filter((ad) => this.testRooms(ad.offer.rooms))
+        .filter((ad) => this.testGuests(ad.offer.guests))
+        .filter((ad) => this.testFeatures(ad.offer.features))
+        .slice(0, limit);
     }
   };
 }
